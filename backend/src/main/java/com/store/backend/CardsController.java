d37eaad3f4b6e346c77af78f81c493819db8e5d7
Aaron W <aaron@empyr.dev>
2020-05-02 02:45:38 -0700

    Removing unused imports

--- backend/src/main/java/com/store/backend/CardsController.java
+++ backend/src/main/java/com/store/backend/CardsController.java
@@ -14,9 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 // Imports all DB Entities
 import com.store.backend.DBConnections.Account.*;
 import com.store.backend.DBConnections.CardInfo.*;
-import com.store.backend.DBConnections.ItemCategories.*;
-import com.store.backend.DBConnections.Items.*;
-import com.store.backend.DBConnections.Orders.*;
 import com.store.backend.DBConnections.*;
 
 @RestController
@@ -29,25 +26,7 @@ public class CardsController {
     private CardInfoRepo cardRepo;
 
     @Autowired
-    private ItemCatRepo itemCatRepo;
-
-    @Autowired
-    private ItemsRepo itemRepo;
-
-    @Autowired
-    private OrdersRepo orderRepo;
-
-    @Autowired
-    private BelongsRepo belongRepo;
-
-    @Autowired
-    private ContainsRepo containRepo;
-
-    @Autowired
     private HoldsRepo holdRepo;
-
-    @Autowired
-    private MakeRepo makeRepo;
 
     @GetMapping("/cards")
     public ArrayList<CardInfo> getCards(
--- backend/src/main/java/com/store/backend/DBConnections/Account/AccountRepo.java
+++ backend/src/main/java/com/store/backend/DBConnections/Account/AccountRepo.java
@@ -2,8 +2,6 @@ package com.store.backend.DBConnections.Account;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 import com.store.backend.DBConnections.Account.Account;
 
 public interface AccountRepo extends CrudRepository<Account, Integer>{
